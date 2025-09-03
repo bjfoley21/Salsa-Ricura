cbf3631362bcc3cb7f0b39f596a7ee8d1fa228cb
bjfoley21 <bjfoley21@gmail.com>
2025-09-02 23:32:29 -0400

    m

---
+++ app/page.js
@@ -0,0 +1,37 @@
+export default function Page() {
+    return (
+      <div className="success-wrapper">
+        <div className="schedule-head">
+          <h2>CLASS SCHEDULE</h2>
+        </div>
+  
+        <div className="schedule">
+          <div>
+            <h2 className="title">Monday</h2>
+            <p>Salsa Partnering Level 1 with Ricardo 7-8pm</p>
+            <p>Bachata Partnering with Ricardo 8-9pm</p>
+            <p>Salsa Partnering Level 2 with Ricardo 9-10pm</p>
+            <br />
+            <h3 className="title">Monday Prices</h3>
+            <p>8-Week Session: $104</p>
+            <p>5-Week Session: $70</p>
+            <p>Drop-In: $15</p>
+            <br />
+            <br />
+  
+            <h2 className="title">Wednesday</h2>
+            <p>Salsa on2 Footwork with Juan Lopez 7-8pm</p>
+            <p>Salsa on2 Partnerwork with Juan Lopez 8-9pm</p>
+            <br />
+            <br />
+            <h3 className="title">Wednesday Prices</h3>
+            <p>Drop-In: $15</p>
+            <p>Both Classes: $20</p>
+            <br />
+            <p>jrca1821@gmail.com</p>
+            <p>(857)348-8362</p>
+          </div>
+        </div>
+      </div>
+    );
+  }

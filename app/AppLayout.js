b1bed07d074c284009454bc32bf6ecaf92d2d653
Brendan <bjfoley21@gmail.com>
2025-09-03 01:01:27 -0400

    m

--- app/AppLayout.js
+++ app/AppLayout.js
@@ -9,6 +9,7 @@ const AppLayout = ({ children }) => {
       <Head>
         <title>Salsa Ricura</title>
         <meta name="description" content='Salsa Ricura is a Salsa and Bachata dance studio located at the Dance Complex in Cambridge. We offer partnerwork classes every Monday and footwork classes every Wednesday'/>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="main-container">
         {children}

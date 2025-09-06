7412302bf6d950c9995fff7992bb57e61a6b60d4
Brendan <bjfoley21@gmail.com>
2025-09-06 00:30:17 -0400

    m

--- app/page.js
+++ app/page.js
@@ -17,11 +17,21 @@ export default function Page() {
             <a href='https://www.mindbodyonline.com/explore/locations/salsa-ricura'>
             <p>Drop-In: $20</p> 
             </a>
+            <a href='https://www.mindbodyonline.com/explore/locations/salsa-ricura'>
             <p>5-Week Session: $85</p>
+            </a>
+            <a href='https://www.mindbodyonline.com/explore/locations/salsa-ricura'>
             <p>10-Week Session: $130</p>
+            </a>
+            <a href='https://www.mindbodyonline.com/explore/locations/salsa-ricura'>
             <p>New Student 10-Week Session: $90</p>
+            </a>
+            <a href='https://www.mindbodyonline.com/explore/locations/salsa-ricura'>
             <p>1-hr private (one students): $60</p>
+            </a>
+            <a href='https://www.mindbodyonline.com/explore/locations/salsa-ricura'>
             <p>1-hr private (two students): $80</p>
+            </a>
             <br />  
           </div>
         </div>

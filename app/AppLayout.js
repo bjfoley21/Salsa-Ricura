ce299b44f1a16727841c38d83d3ca04268bdd69a
Brendan <bjfoley21@gmail.com>
2025-09-10 18:57:43 -0400

    m

--- app/AppLayout.js
+++ app/AppLayout.js
@@ -10,7 +10,16 @@ const AppLayout = ({ children }) => {
         <title>Salsa Ricura</title>
         <meta name="description" content='Salsa Ricura is a NY style On2 Salsa and Bachata dance studio located at the Dance Complex in Cambridge. We offer partnerwork and footwork classes every Monday'/>
         <link rel="icon" href="/favicon.ico" />
-        
+        {/* Google Tag for AW-17532732844 */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17532732844"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17532732844');
+          `
+        }} />
       </Head>
       <main className="main-container">
         {children}
